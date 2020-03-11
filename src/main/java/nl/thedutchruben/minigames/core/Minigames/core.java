e59f6d01881067dfa132abe8d9d9e703cd8532a7
Ruben de Roos <deroosr@gmail.com>
2020-03-11 17:19:03 +0100

    add core file

---
+++ src/main/java/nl/thedutchruben/minigames/core/Minigames/core.java
@@ -0,0 +1,17 @@
+package nl.thedutchruben.minigames.core.Minigames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
